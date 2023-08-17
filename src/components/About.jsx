9883d1fee00939112cf2e3a521550e8c8d1c3bf3
zensei76 <bhargavburgul@gmail.com>
2023-08-17 18:56:07 +0530

    minor styling changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const ServiceCard = ({ index, title, icon }) => {
         variants={fadeIn("right", "spring", index * 0.5, 0.75)}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
       >
-        
+
         <div
           options={{
             max: 45,
@@ -42,33 +42,7 @@ const ServiceCard = ({ index, title, icon }) => {
     </Tilt>
   );
 };
-// const About = () => {
-//   return (
-// <>
-//   <motion.div variants={textVariant()}>
-//     <p className={styles.sectionSubText}>Introduction</p>
-//     <h2 className={styles.sectionHeadText}>Overview.</h2>
-//   </motion.div>
 
-//   <motion.p
-//     variants={fadeIn("", "", 0.1, 1)}
-//     className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-//   >
-//     I'm a skilled software developer with experience in TypeScript and
-//     JavaScript, and expertise in frameworks like React, Node.js, and
-//     Three.js. I'm a quick learner and collaborate closely with clients to
-//     create efficient, scalable, and user-friendly solutions that solve
-//     real-world problems. Let's work together to bring your ideas to life!
-//   </motion.p>
-
-//   <div className='mt-20 flex flex-wrap gap-10'>
-//     {services.map((service, index) => (
-//       <ServiceCard key={service.title} index={index} {...service} />
-//     ))}
-//   </div>
-// </>
-//   );
-// };
 const About = () => {
   return (
     <AnimatePresence>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,23 +20,24 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Bhargav</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develope Softwares , Websites <br className='sm:block hidden ' />
-            Full Stack  |  Frontend
+            web developer.
+            <br className='sm:block hidden ' />
           </p>
+          <p className=" text-[12px] text-white opacity-50 ">Full Stack   |  Frontend</p>
         </div>
       </div>
       <ComputersCanvas />
       <div className='absolute xs:bottom-5 bottom-32 w-full flex justify-center items-center'>
-        <a href="#about">
+        <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2  '>
-            <motion.div 
-            animate= {{y: [0,24,0]}}
-            transition={{
-              duration: 1.5,
-              repeat: Infinity,
-              repeatType : 'loop',
-            }}
-            className="w-3 h-3 rounded-full bg-secondary mb-1"
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
           </div>
         </a>
